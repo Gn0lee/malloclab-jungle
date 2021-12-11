6f3598320464adccc83435bacb76049d3bd2a271
Gnolee <jinho605@gmail.com>
2021-12-11 08:19:28 +0000

    nextfit_fail

--- mm.c
+++ mm.c
@@ -189,10 +189,13 @@ static void *coalesce(void *bp){
 }
 
 static void *find_fit(size_t asize){
-    void *bp;
-
+    char *bp;
+    char temp;
     for(bp = heap_listp; GET_SIZE(HDRP(bp))>0;bp = NEXT_BLKP(bp)){
         if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))){
+            temp = *bp;
+            * heap_listp = temp;
+            heap_listp = bp;
             return bp;
         }
     }
